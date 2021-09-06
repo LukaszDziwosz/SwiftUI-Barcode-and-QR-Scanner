463fe3e5274b6f2495268e7ec0204b872a793c2c
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-06 20:08:01 +0200

    Initial Commit

---
+++ SwiftUIBarcodeScanner/SwiftUIBarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBarcodeScannerApp.swift
+//  SwiftUIBarcodeScanner
+//
+//  Created by Lukasz Dziwosz on 06/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
